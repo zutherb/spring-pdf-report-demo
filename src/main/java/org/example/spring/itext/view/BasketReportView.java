1583444164e9c756210706d8433f1ef9c48bf174
Bernd Zuther <bernd.zuther@comsysto.com>
2011-11-19 16:53:19 +0100

    add html support for itext

--- src/main/java/org/example/spring/itext/view/BasketReportView.java
+++ src/main/java/org/example/spring/itext/view/BasketReportView.java
@@ -1,8 +1,11 @@
 package org.example.spring.itext.view;
 
 import com.lowagie.text.Document;
+import com.lowagie.text.Element;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.Table;
+import com.lowagie.text.html.simpleparser.HTMLWorker;
+import com.lowagie.text.html.simpleparser.StyleSheet;
 import com.lowagie.text.pdf.PdfWriter;
 import org.example.spring.jasperreport.service.basket.api.Basket;
 import org.example.spring.jasperreport.service.basket.apimodel.BasketItem;
@@ -13,6 +16,9 @@ import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Component("itextBasketReport")
@@ -43,7 +49,10 @@ public class BasketReportView extends AbstractPdfView {
 
         document.add(basketItemTable);
 
-        document.add(new Paragraph(cmsRessource.getFooter()));
-
+        String footer = cmsRessource.getFooter();
+        List<Element> arrayList = HTMLWorker.parseToList(new StringReader(footer), new StyleSheet());
+        for(Element element : arrayList){
+            document.add(element);
+        }
     }
 }
